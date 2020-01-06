75aa8c1150683e67bd0d1df123316106e712d704
williamswill <willoymaniak@gmail.com>
2020-01-06 20:28:50 +0100

    Updates

---
+++ precache-manifest.59dcfaf9d62b3bc33ec03f8f18f35dc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2ed94080008fee1d4ce19fe3cf0db7",
+    "url": "/stockings/index.html"
+  },
+  {
+    "revision": "9810658034f5276318fc",
+    "url": "/stockings/static/css/main.4e4d00e8.chunk.css"
+  },
+  {
+    "revision": "e891f720a5c9e001a0af",
+    "url": "/stockings/static/js/2.a35765fb.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/stockings/static/js/2.a35765fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9810658034f5276318fc",
+    "url": "/stockings/static/js/main.c686f578.chunk.js"
+  },
+  {
+    "revision": "07e9f697993d160aff37",
+    "url": "/stockings/static/js/runtime-main.5e1b462d.js"
+  },
+  {
+    "revision": "87e9e32fa7d3b4efd99cc181236613c8",
+    "url": "/stockings/static/media/landing_bg.87e9e32f.jpg"
+  }
+]);
